22b629202cb71f9c1edc6078d7cb5cd5ec43ef74
Rayan Tabate <tabaterayanr@gmail.comgit config -- global user.email tabaterayanr@gmail.comgit config --global user.name Rayan>
2026-05-24 16:01:23 +0200

    Modification du projet Android GSB

--- app/src/main/java/com/example/expo/MainActivity.java
+++ app/src/main/java/com/example/expo/MainActivity.java
@@ -3,11 +3,11 @@ package com.example.expo;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,14 +20,18 @@ public class MainActivity extends AppCompatActivity {
     EditText codePostalInput;
     EditText rechercheInput;
 
-    EditText dateInput;
     EditText heureInput;
     EditText idProInput;
 
+    CalendarView calendarView;
+
     TextView textListe;
+
+    String dateChoisie = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -37,97 +41,178 @@ public class MainActivity extends AppCompatActivity {
         prenomInput = findViewById(R.id.editTextPrenom);
         typeInput = findViewById(R.id.editTextType);
         villeInput = findViewById(R.id.editTextVille);
-        codePostalInput = findViewById(R.id.editTextCodePostal);
-        rechercheInput = findViewById(R.id.editTextRecherche);
 
-        dateInput = findViewById(R.id.editTextDate);
-        heureInput = findViewById(R.id.editTextHeure);
-        idProInput = findViewById(R.id.editTextIdPro);
+        codePostalInput =
+                findViewById(R.id.editTextCodePostal);
 
-        textListe = findViewById(R.id.textViewListe);
+        rechercheInput =
+                findViewById(R.id.editTextRecherche);
+
+        heureInput =
+                findViewById(R.id.editTextHeure);
+
+        idProInput =
+                findViewById(R.id.editTextIdPro);
+
+        calendarView =
+                findViewById(R.id.calendarView);
+
+        textListe =
+                findViewById(R.id.textViewListe);
+
+
+        calendarView.setOnDateChangeListener(
+                (view, year, month, day) -> {
+
+                    dateChoisie =
+                            day + "/"
+                                    + (month+1)
+                                    + "/"
+                                    + year;
+                }
+        );
+
     }
 
 
-    public void insertionClic(View view) {
+
+    public void insertionClic(View view){
+
         db.insertData(
+
                 nomInput.getText().toString(),
+
                 prenomInput.getText().toString(),
+
                 typeInput.getText().toString(),
+
                 villeInput.getText().toString(),
+
                 codePostalInput.getText().toString()
+
         );
 
-        textListe.setText("Professionnel ajouté");
+        textListe.setText(
+                "Professionnel ajouté"
+        );
+
     }
 
 
-    public void afficherClic(View view) {
-        Cursor data = db.getAllData();
 
-        String texte = "";
+    public void afficherClic(View view){
 
-        while (data.moveToNext()) {
-            texte = texte
-                    + "ID : " + data.getString(0) + " - "
-                    + data.getString(1) + " "
-                    + data.getString(2) + " - "
-                    + data.getString(3) + " - "
-                    + data.getString(4) + " - "
-                    + data.getString(5)
-                    + "\n";
+        Cursor data =
+                db.getAllData();
+
+        String texte="";
+
+        while(data.moveToNext()){
+
+            texte =
+                    texte
+                            + data.getString(1)
+                            +" "
+                            + data.getString(2)
+                            +"\n";
+
         }
 
         textListe.setText(texte);
+
     }
 
 
-    public void rechercheClic(View view) {
-        Cursor data = db.searchData(
-                rechercheInput.getText().toString()
-        );
 
-        String texte = "";
+    public void rechercheClic(View view){
 
-        while (data.moveToNext()) {
-            texte = texte
-                    + "ID : " + data.getString(0) + " - "
-                    + data.getString(1) + " "
-                    + data.getString(2) + " - "
-                    + data.getString(4) + " - "
-                    + data.getString(5)
-                    + "\n";
+        Cursor data =
+                db.searchData(
+
+                        rechercheInput
+                                .getText()
+                                .toString()
+
+                );
+
+        String texte="";
+
+        while(data.moveToNext()){
+
+            texte =
+                    texte
+                            + data.getString(1)
+                            +" "
+                            + data.getString(2)
+                            +"\n";
+
         }
 
         textListe.setText(texte);
+
     }
 
 
-    public void rdvClic(View view) {
+
+
+    public void rdvClic(View view){
+
         db.insertRDV(
-                dateInput.getText().toString(),
-                heureInput.getText().toString(),
-                Integer.parseInt(idProInput.getText().toString())
+
+                dateChoisie,
+
+                heureInput
+                        .getText()
+                        .toString(),
+
+                Integer.parseInt(
+
+                        idProInput
+                                .getText()
+                                .toString()
+
+                )
+
         );
 
-        textListe.setText("RDV ajouté");
+        textListe.setText(
+                "RDV ajouté"
+        );
+
     }
 
 
-    public void planningClic(View view) {
-        Cursor data = db.getPlanning(
-                dateInput.getText().toString()
-        );
 
-        String texte = "";
 
-        while (data.moveToNext()) {
-            texte = texte
-                    + "RDV : " + data.getString(1)
-                    + " à " + data.getString(2)
-                    + " avec professionnel ID : " + data.getString(3)
-                    + "\n";
+
+    public void planningClic(View view){
+
+        Cursor data =
+                db.getPlanning(
+                        dateChoisie
+                );
+
+        String texte="";
+
+        while(data.moveToNext()){
+
+            texte =
+                    texte
+                            +"Date : "
+                            +data.getString(1)
+
+                            +" Heure : "
+
+                            +data.getString(2)
+
+                            +"\n";
+
         }
 
-        textListe.setText(texte);
+        textListe.setText(
+                texte
+        );
+
     }
+
 }
